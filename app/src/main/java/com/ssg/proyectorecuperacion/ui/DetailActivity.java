8d3fe97f46f5da83d78fb2693fa7ac90e2143567
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-03-31 13:01:36 +0200

    Receive data in DetailActivity

--- app/src/main/java/com/ssg/proyectorecuperacion/ui/DetailActivity.java
+++ app/src/main/java/com/ssg/proyectorecuperacion/ui/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.ssg.proyectorecuperacion.ui;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,35 @@ import com.ssg.proyectorecuperacion.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView title, author, year, category, status;
+    ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        title = findViewById(R.id.txtTitleDetail);
+        author = findViewById(R.id.txtAuthorDetail);
+        year = findViewById(R.id.txtYearDetail);
+        category = findViewById(R.id.txtCategoryDetail);
+        status = findViewById(R.id.txtStatusDetail);
+        image = findViewById(R.id.imgBookDetail);
+
+        // Recibir datos
+        String t = getIntent().getStringExtra("title");
+        String a = getIntent().getStringExtra("author");
+        String y = getIntent().getStringExtra("year");
+        String c = getIntent().getStringExtra("category");
+        String s = getIntent().getStringExtra("status");
+
+        // Mostrar datos
+        title.setText(t);
+        author.setText(a);
+        year.setText(y);
+        category.setText(c);
+        status.setText(s);
+
     }
 }

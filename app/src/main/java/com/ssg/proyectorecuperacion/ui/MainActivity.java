6bee76c55a19605328517319852d5b5a8566b031
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-03-31 16:27:26 +0200

    Connect API in MainActivity

--- app/src/main/java/com/ssg/proyectorecuperacion/ui/MainActivity.java
+++ app/src/main/java/com/ssg/proyectorecuperacion/ui/MainActivity.java
@@ -13,9 +13,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ssg.proyectorecuperacion.R;
 import com.ssg.proyectorecuperacion.adapter.BookAdapter;
 import com.ssg.proyectorecuperacion.model.Book;
+import com.ssg.proyectorecuperacion.model.BookResponse;
+import com.ssg.proyectorecuperacion.network.ApiService;
+import com.ssg.proyectorecuperacion.network.RetrofitClient;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,13 +35,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         listBooks = new ArrayList<>();
-
         adapter = new BookAdapter(listBooks, this);
         recyclerView.setAdapter(adapter);
+
+        ApiService apiService = RetrofitClient.getClient().create(ApiService.class);
+
+        Call<BookResponse> call = apiService.searchBooks("harry potter");
+
+        call.enqueue(new Callback<BookResponse>() {
+            @Override
+            public void onResponse(Call<BookResponse> call, Response<BookResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    listBooks.clear();
+                    listBooks.addAll(response.body().getDocs());
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BookResponse> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

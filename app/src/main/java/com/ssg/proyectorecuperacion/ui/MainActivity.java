942122f1e541ca6c58ea008ed6977fcd1e8c26ce
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-05 11:51:50 +0200

    Change key to bookKey

--- app/src/main/java/com/ssg/proyectorecuperacion/ui/MainActivity.java
+++ app/src/main/java/com/ssg/proyectorecuperacion/ui/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         adapter.setOnItemClickListener(book -> {
             Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-            intent.putExtra("key", book.getKey());
+            intent.putExtra("bookKey", book.getKey());
             intent.putExtra("cover", book.getCoverUrl());
             startActivity(intent);
         });

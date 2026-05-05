02fbcd5b0cda187a42b67b671bc75df59761b42f
Sara Silva <silvag24@fpcoruna.afundacion.org>
2026-05-05 11:17:12 +0200

    Add endpoint in ApiService

--- app/src/main/java/com/ssg/proyectorecuperacion/network/ApiService.java
+++ app/src/main/java/com/ssg/proyectorecuperacion/network/ApiService.java
@@ -1,12 +1,16 @@
 package com.ssg.proyectorecuperacion.network;
 
+import com.ssg.proyectorecuperacion.model.BookDetail;
 import com.ssg.proyectorecuperacion.model.BookResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 public interface ApiService {
 
     @GET("search.json")
     Call<BookResponse> searchBooks(@Query("q") String query);
+    @GET("works/{id}.json")
+    Call<BookDetail> getBookDetail(@Path("id") String id);
 }
